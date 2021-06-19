b941ec6175d4e342a7fa504efc2a6730c994adc1
Dan Nicolau <dan.nicolau@protonmail.com>
2021-06-19 01:21:19 -0400

    update gitignore, can log in

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+require('dotenv').config()
+const got = require('got');
 
+const url = process.env.ENDPOINT;
+
+async function login(){
+    console.log('Attempting login');
+
+    const { body } = await got.post(url + '/account/login', {
+        json: {
+            employeeNumber: process.env.EMPLOYEE_NUMBER,
+            password: process.env.PASSWORD
+        },
+        responseType: 'json'
+    });
+
+    console.log(body);
+
+}
 
 if (require.main === module) {
-    
+    login()
 }

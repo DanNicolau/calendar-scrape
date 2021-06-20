01af70f825be0141582f8407a506e1844902f09b
dnicolau <dnicolau@protonmail.com>
2021-06-20 00:08:52 -0400

    actually gets the calendar data

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 require('dotenv').config()
 const got = require('got');
-
+const { CookieJar } = require('tough-cookie');
 const url = process.env.ENDPOINT;
+
+const cookieJar = new CookieJar();
 
 async function login(){
     console.log('Attempting login');
 
-    const { body } = await got.post(url + '/account/login', {
+    const res = await got.post(url + '/account/login', {
         json: {
             employeeNumber: process.env.EMPLOYEE_NUMBER,
             password: process.env.PASSWORD
         },
-        responseType: 'json'
+        responseType: 'json',
+        cookieJar: cookieJar
     });
-
-    console.log(body);
-
+    return res;
 }
 
-function thisMonth(){
-    
-}
-
-async function getCalendar(){
+async function getCalendar(prev_res){
     console.log('Accessing calendar');
-    const { body } = await got.post(url + '/api/Calendar', {
-        json: {
+    const payload = {
             countryCode: process.env.COUNTRY_CODE,
-            month: "2021-06-01 00:00:00",
-            staffId: 0,
-            storeId: 0
-        },
-        responseType: 'json'
+            month: new Date().toISOString().substr(0, 8) + '01 00:00:00',
+            staffId: prev_res.body.result.data.staffId,
+            storeId: prev_res.body.result.data.storeId
+    }
+    const res = await got.post(url + '/api/Calendar', {
+        json: payload,
+        cookieJar: cookieJar
     });
+    return res;
 }
 
 if (require.main === module) {
-    login()
-        .then(getCalendar);
+    (async () => { 
+        const login_res    = await login();
+        const calendar_res = await getCalendar(login_res);
+        const calendar_data = JSON.parse(calendar_res.body.substr(1)).res.result.data;
+        console.log(calendar_data);
+    })();
 }

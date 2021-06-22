96e9503dc501c443a4d85e6be25569fbd64ba5e7
Dan Nicolau <dan.nicolau@protonmail.com>
2021-06-21 21:22:42 -0400

    exports ics

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const got = require('got');
 const { CookieJar } = require('tough-cookie');
 const ics = require('ics');
+const { writeFileSync } = require('fs');
 
 const url = process.env.ENDPOINT;
 const cookieJar = new CookieJar();
@@ -20,32 +21,36 @@ async function login(){
     return res;
 }
 
-function parseDate(str){
-    try {
-        const dateArr = str.split('-');
-        const [seg0, seg1] = dateArr;
-        switch (dateArr.length){
-            case 1: {
-                const monthTwoDigits = ('0' + seg0).substr(-2);
-                return new Date().toISOString().substr(0, 5) + monthTwoDigits + '-01 00:00:00'; 
-                break;
+function generateRequestDate(str){
+    if (str) {
+        try {
+            const dateArr = str.split('-');
+            const [seg0, seg1] = dateArr;
+            switch (dateArr.length){
+                case 1: {
+                    const monthTwoDigits = ('0' + seg0).substr(-2);
+                    return new Date().toISOString().substr(0, 5) + monthTwoDigits + '-01 00:00:00'; 
+                    break;
+                }
+                case 2: {
+                    const monthTwoDigits = ('0' + seg1).substr(-2);
+                    return seg0 + '-' + monthTwoDigits + '-01 00:00:00';
+                    break;
+                }
+                case 3: {
+                    const monthTwoDigits = ('0' + seg1).substr(-2);
+                    return seg0 + '-' + monthTwoDigits + '-01 00:00:00';
+                    break;
+                }
+                default:
+                    throw new Error('Unhandled number of segments in date parsing');
             }
-            case 2: {
-                const monthTwoDigits = ('0' + seg1).substr(-2);
-                return seg0 + '-' + monthTwoDigits + '-01 00:00:00';
-                break;
-            }
-            case 3: {
-                const monthTwoDigits = ('0' + seg1).substr(-2);
-                return seg0 + '-' + monthTwoDigits + '-01 00:00:00';
-                break;
-            }
-            default:
-                throw new Error('Unhandled number of segments in date parsing');
-        }
 
-    } catch (err) {
-        console.log(err);
+        } catch (err) {
+            console.log(err);
+            return new Date().toISOString().substr(0, 8) + '01 00:00:00';
+        }
+    } else {
         return new Date().toISOString().substr(0, 8) + '01 00:00:00';
     }
 }
@@ -75,22 +80,78 @@ function printCalendar(calendarData, dateHR){
 
     calendarList.forEach(el => {
         if (el.workingTime) {
-            console.log(el.dates, el.workingTime, el.clockOutTime);
+            console.log(el.dates, el.workingTime.split('T')[1], el.clockOutTime.split('T')[1], el.workSegmentCode);
+        }
+    });
+}
+
+function getTitle(el){
+    switch (el.workSegmentCode) {
+        case 'D01':
+            return 'Work';
+            break;
+        case 'D05':
+            return 'Business Trip and Training';
+            break;
+        default:
+            return el.workSegmentCode;
+    }
+}
+
+function calculateICSStart(el){
+    const [yearStr, monthStr] = generateRequestDate(process.argv[2]).split('-').slice(0, 2); 
+    const year = parseInt(yearStr);
+    const month = parseInt(monthStr);
+    const date = el.dates;
+    const [hourStr, minuteStr] = el.workingTime.split('T')[1].split(':');
+    const hour = parseInt(hourStr);
+    const minute = parseInt(minuteStr);
+    return [year, month, date, hour, minute]; 
+}
+
+function calculateDuration(el){
+    const startTime = Date.parse(el.workingTime);
+    const endTime = Date.parse(el.clockOutTime);
+    const totalMinutes = (endTime - startTime) / 1000 / 60;
+    const minutes = totalMinutes % 60;
+    const hours = (totalMinutes - minutes) / 60;
+    return { hours, minutes };
+}
+
+function generateICS(calendarData){
+    const { storeName, calendarList } = calendarData;
+    let events = [];
+    calendarList.forEach( el => {
+        if (el.workingTime) {
+            events.push({
+                title: getTitle(el),
+                start: calculateICSStart(el),
+                duration: calculateDuration(el)
+            });
+        }
+    });
+    console.log(events);
+
+    ics.createEvents(events, (err, value) => {
+        if (err) {
+            console.log(err);
+        } else {
+            writeFileSync(`${__dirname}/scraped_calendar.ics`, value)
         }
     });
 }
 
 if (require.main === module) {
 
-    const dateString = parseDate(process.argv[2]);
+    const dateString = generateRequestDate(process.argv[2]);
     // const dateHR = dateString.substr(0, 7);
 
     (async () => { 
         const loginRes    = await login();
-        const calendarRes = await getCalendar(loginRes, parseDate(process.argv[2]));
+        const calendarRes = await getCalendar(loginRes, generateRequestDate(process.argv[2]));
         const calendarData = JSON.parse(calendarRes.body.substr(1)).res.result.data;
         // const { calendarList } = calendar_data;
-
         printCalendar(calendarData, dateString.substr(0, 7));
+        generateICS(calendarData);
     })();
 }
